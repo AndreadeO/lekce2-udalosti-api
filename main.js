3c7a5882a1c23ebcf322a48a0c1da137bf762b58
Andrea de Oliveira <soudkova.a@gmail.com>
2022-02-15 18:52:23 +0100

    using the classlist and add/remove class functions

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 // tady je místo pro náš program
 
 console.log('Ahoj');
+
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty');
+
+let ctverecek = document.querySelector('div.zluty');
+ctverecek.classList.add('ctverecek');
+
